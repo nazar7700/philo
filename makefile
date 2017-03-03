4851cb53a1bbda2451d8eba69cb2fa6f03aa53fc
Nazar Leonidovich Stelmakh <nazar.stelmakh@wsu.edu>
2017-03-03 11:49:46 -0800

    Started working on project. Need to get philosopher to do things.

--- makefile
+++ makefile
@@ -1,10 +1,14 @@
 #OPTIMIZE=-03 -DNDEBUG
 DEBUGGABLE=-g
 CFLAGS = -std=c99 -Wall -pedantic $(DEBUGGABLE) $(OPTIMIZE) -c -g
-philo: philo.o
+philo: philo.o random.o
 	gcc philo.o -o philo
-philo.o:
+philo.o: philo.c random.h
 	gcc $(CFLAGS) philo.c
+
+random.o: random.c random.h
+	gcc -lm $(CFLAGS) random.c 
+
 clean:
 	rm -f *.o philo
 	
--- philo.c
+++ philo.c
@@ -1,4 +1,45 @@
+// Nazar Stelmakh
+// Assignment 6 - Philosopher Problem
+// 23 March 2017
+#define _XOPEN_SOURCE
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include "random.h"
+
+#define NUM_PHILO 5
+
+
+void philo(){
+
+    pid_t pid[NUM_PHILO];
+
+    for (int i = 0; i < NUM_PHILO; ++i){
+        pid[i] = fork();
+        if (pid[i] < 0){
+            printf("Error\n");
+            exit(1);
+        }else if (pid[i] == 0){
+            printf("Child (%d): %d\n", i + 1, getpid());
+            //call something that behaves like a philosopher
+            exit(1);
+        }        
+    }
+    for (int i = 0; i < NUM_PHILO; ++i){
+        wait(NULL);
+    }
+}
+
+
 int main(int argc, char* argv[]){
+
+    philo();
 
     return 0;
 }
